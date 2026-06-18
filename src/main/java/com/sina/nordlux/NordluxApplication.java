c56c2df1f4139e47aa3508d0eaf3b1d5edd4205f
sinahaha <116685483+sinahaha@users.noreply.github.com>
2026-06-18 21:03:16 +0200

    Add files via upload

---
+++ src/main/java/com/sina/nordlux/NordluxApplication.java
@@ -0,0 +1,13 @@
+package com.sina.nordlux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NordluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NordluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sina/nordlux/NordluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sina.nordlux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NordluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
